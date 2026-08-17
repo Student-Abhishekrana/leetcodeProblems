83484aba6f2cef83d92036e95ee9ccf67cfe013c
Abhishek Rana <abhishekrana6974@gmail.com>
2026-08-17 14:58:38 +0530

    Time: 0 ms (100%), Space: 42 MB (55.29%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        //tabulation
+        int []ways = new int[n+1];
+
+        ways[0]=1;
+        ways[1] =1;
+      
+
+        for(int i=2; i<=n; i++){
+           
+
+            ways[i] =ways[i-1]+ways[i-2];
+            
+
+        }
+        return ways[n];
+    }
+}
