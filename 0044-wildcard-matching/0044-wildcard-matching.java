54ff9381bef047fc53a4391f0bce019665f351c1
Abhishek Rana <abhishekrana6974@gmail.com>
2026-08-26 16:14:07 +0530

    Time: 54 ms (24.53%), Space: 47.5 MB (49.95%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        //empty String with empty pattern
+        dp[0][0] = true;
+
+        //empty string with pattern containing *
+        for (int j = 1; j < m + 1; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                //character of string and pattern are same
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                }
+                //* can replace empty "" & sequence of character
+                 else if (p.charAt(j - 1) == '*') {
+
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
+}
