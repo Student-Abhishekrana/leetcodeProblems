03281b3a80c993e192d5c5dbe56c8cba754f69b0
Abhishek Rana <abhishekrana6974@gmail.com>
2026-09-10 12:06:36 +0530

    Time: 69 ms (30.38%), Space: 46.7 MB (27.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,17 +1,14 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-
-        //for remove duplicate items
+        //increasing Order
         HashSet<Integer> set = new HashSet<>();
-       for(int i=0; i<nums.length;i++){
-        set.add(nums[i]);
-       }
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
         int[] arr = new int[set.size()];
-
         int i = 0;
-        for (int num : set) {
-            arr[i] = num;
-            i++;
+        for (Integer num : set) {
+            arr[i++] = num;
         }
         Arrays.sort(arr);
 
@@ -20,30 +17,26 @@ class Solution {
     }
 
     private int lcs(int[] nums, int[] arr) {
-        if (nums.length < arr.length) {
-            return lcs(arr, nums);
-        }
-
         int n = nums.length;
         int m = arr.length;
 
-        int[][] dp = new int[n+1][m + 1];
-     
-        for (int i = 1; i <= n; i++) {
+        if (n > m) {
+            return lcs(arr, nums);
+        }
 
-            for (int j = 1; j <= m; j++) {
-          
+        int[] dp = new int[m + 1];
+        for (int i = 1; i < n + 1; i++) {
+            int diag = 0;
+            for (int j = 1; j < m + 1; j++) {
+                int temp = dp[j];
                 if (nums[i - 1] == arr[j - 1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[j] = temp + 1;
                 } else {
-                   int ans1 =dp[i-1][j];
-                   int ans2 =dp[i][j-1];
-                   dp[i][j] =Math.max(ans1,ans2);
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
                 }
-
-              
+                diag = temp;
             }
         }
-        return dp[n][m];
+        return dp[m];
     }
 }
